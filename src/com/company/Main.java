143c0acd2fb364b7fd2891a6585a72a927994963
chris <chri402s@stud.kea.dk>
2021-09-25 12:07:54 +0200

    start adventure del1 rooms virker

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,120 +16,103 @@ public class Main {
         Room room7 = new Room("room7");
         Room room8 = new Room("room8");
         Room room9 = new Room("room9");
-        Room currentRoom = null;
+
+        room1.setEast(room2);
+        room1.setSouth(room4);
+        room2.setWest(room1);
+        room2.setEast(room3);
+        room3.setWest(room2);
+        room3.setSouth(room6);
+        room4.setNorth(room1);
+        room4.setSouth(room7);
+        room5.setSouth(room8);
+        room6.setNorth(room3);
+        room6.setSouth(room9);
+        room7.setNorth(room4);
+        room7.setEast(room8);
+        room8.setNorth(room5);
+        room8.setEast(room9);
+        room8.setWest(room7);
+        room9.setNorth(room6);
+        room9.setWest(room8);
+
+        Room currentRoom = room1;
+
         //  print velkomst
         System.out.println("Welcome to Adventure");
         System.out.println("introduction, go north, go south, go east, go west");
         System.out.println("Room intro"); //Room1 intro lav variabel
         System.out.println("what do you want to do?");
 
-        userInput  = scanner.nextLine();
-        //rum1
-        if ("go north".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go east".equals(userInput)){
-            System.out.println("going east");// lave koden til at gå til rum 2
-        } else if( "go south".equals(userInput)){
-            System.out.println("going south");// lave koden til at gå til rum 4
-        } else if("go west".equals(userInput)){
-            System.out.println("you hit a wall");
 
-            //rum2
-        } else if ("go north".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go east".equals(userInput)){
-            System.out.println("going east");// lave koden til at gå til rum 3
-        } else if( "go south".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if("go west".equals(userInput)){
-            System.out.println("going west");// lave koden til at gå til rum 1
+        // Direktion handlinger
+        boolean gameOn = true;
+        while (gameOn) {
+            userInput = scanner.nextLine();
+            if ("go north".equals(userInput)) {
+                if (currentRoom.getNorth() == null) {
+                    System.out.println("You cannot go that way");
+                } else {
+                    System.out.println("Going north");
+                    currentRoom = currentRoom.getNorth();
+                    System.out.println(currentRoom.getDescription());
 
-            //rum3
-        } else if ("go north".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go east".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go south".equals(userInput)){
-            System.out.println("going south");// lave koden til at gå til rum 6
-        } else if("go west".equals(userInput)){
-            System.out.println("going west");// lave koden til at gå til rum 2
+                }
 
-            //rum4
-        } else if ("go north".equals(userInput)){
-            System.out.println("going north");// lave koden til at gå til rum 1
-        } else if( "go east".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go south".equals(userInput)){
-            System.out.println("going south");// lave koden til at gå til rum 7
-        } else if("go west".equals(userInput)){
-            System.out.println("you hit a wall");
+            } else if ("go east".equals(userInput)) {
+                if (currentRoom.getEast() == null) {
+                    System.out.println("You cannot go that way");
+                } else {
+                    System.out.println("Going east");
+                    currentRoom = currentRoom.getEast();
+                    System.out.println(currentRoom.getDescription());
+                }
 
-            //rum5
-        } else if ("go north".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go east".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go south".equals(userInput)){
-            System.out.println("going south");// lave koden til at gå til rum 8
-        } else if("go west".equals(userInput)){
-            System.out.println("you hit a wall");
+            } else if ("go south".equals(userInput)) {
+                if (currentRoom.getSouth() == null) {
+                    System.out.println("You cannot go that way");
+                } else {
+                    System.out.println("Going south");
+                    currentRoom = currentRoom.getSouth();
+                    System.out.println(currentRoom.getDescription());
+                }
 
-            //rum6
-        } else if ("go north".equals(userInput)){
-            System.out.println("going north");// lave koden til at gå til rum 3
-        } else if( "go east".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go south".equals(userInput)){
-            System.out.println("going south");// lave koden til at gå til rum 9
-        } else if("go west".equals(userInput)){
-            System.out.println("you hit a wall");
+            } else if ("go west".equals(userInput)) {
+                if (currentRoom.getWest() == null) {
+                    System.out.println("You cannot go that way");
+                } else {
+                    System.out.println("Going west");
+                    currentRoom = currentRoom.getWest();
+                    System.out.println(currentRoom.getDescription());
+                }
 
-            //rum7
-        } else if ("go north".equals(userInput)){
-            System.out.println("going north");// lave koden til at gå til rum 4
-        } else if( "go east".equals(userInput)){
-            System.out.println("going east");// lave koden til at gå til rum 8
-        } else if( "go south".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if("go west".equals(userInput)){
-            System.out.println("you hit a wall");
+                // andre handlinger
+            } else if ("look".equals(userInput)) {
+                System.out.println(currentRoom.getDescription());
 
-            //rum8
-        } else if ("go north".equals(userInput)){
-            System.out.println("going north");// lave koden til at gå til rum 5
-        } else if( "go east".equals(userInput)){
-            System.out.println("going east");// lave koden til at gå til rum 9
-        } else if( "go south".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if("go west".equals(userInput)){
-            System.out.println("going west");// lave koden til at gå til rum 7
+            } else if ("help".equals(userInput)) {
+                System.out.println("""
+                        You can use the following commands: 
+                        go north
+                        go east
+                        go south
+                        go west
+                        look
+                        help
+                        exit
+                        """);
 
-            //rum9
-        } else if ("go north".equals(userInput)){
-            System.out.println("going north");// lave koden til at gå til rum 6
-        } else if( "go east".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if( "go south".equals(userInput)){
-            System.out.println("you hit a wall");
-        } else if("go west".equals(userInput)){
-            System.out.println("going west");// lave koden til at gå til rum 8
+            } else if ("exit".equals(userInput)) {
+                gameOn = false;
+            } else {
+                System.out.println("What do you mean? I dont know what " + userInput + " is");
+            }
 
-            // andre handlinger
-        } else if ("look".equals(userInput)){
-            // print room intro variabel
-        } else if ("help".equals(userInput)){
-            // print handlinge beskrivelse variabel
-        } else if ("exit".equals(userInput)){
-            // lav exit program
-        } else {
-            // print ved ikke hvad brugeren mener
         }
 
-
-
-
-
-
-
     }
+
+
 }
+
+
--- src/com/company/Room.java
+++ src/com/company/Room.java
@@ -2,9 +2,52 @@ package com.company;
 
 public class Room {
     private String name;
+    private String description;
+    private Room north;
+    private Room east;
+    private Room south;
+    private Room west;
 
     public Room (String name){
         this.name = name;
-
     }
+
+    public String getDescription(){
+        return "ddd";
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+        String room1intro = "in room 1";
+    }
+
+    public void setNorth(Room north){
+        this.north = north;
+    }
+    public void setEast(Room east){
+        this.east = east;
+    }
+    public void setSouth(Room south){
+        this.south = south;
+    }
+    public void setWest(Room west){
+        this.west = west;
+    }
+
+    public Room getNorth(){
+        return north;
+    }
+
+    public Room getEast(){
+        return east;
+    }
+
+    public Room getSouth(){
+        return south;
+    }
+
+    public Room getWest(){
+        return west;
+    }
+
 }
