98ce0b27910bad1cbac07b4afb74b01675238912
chris <chri402s@stud.kea.dk>
2021-09-28 14:14:18 +0200

    moveTo metode, og Colour class

--- src/com/company/Adventure.java
+++ src/com/company/Adventure.java
@@ -12,65 +12,31 @@ public class Adventure {
 
     Room currentRoom = map.getStarterRoom();
 
-    String resetColour = "\u001B[0m";
-    String black = "\u001B[30m";
-    String red = "\u001B[31m";
-    String green = "\u001B[32m";
-    String yellow = "\u001B[33m";
-    String blue = "\u001B[34m";
-    String purple = "\u001B[35m";
-    String cyan = "\u001B[36m";
-    String white = "\u001B[37m";
-
         public void play() {
-            System.out.println(cyan + "Welcome to Adventure.\n" + purple +
+            System.out.println(Colours.cyan + "Welcome to Adventure.\n" + Colours.purple +
                     "You'll be traveling through maze of rooms,\n" +
                     "you're objective is to locate the treasure room\n" +
                     "Be careful not to get lost, in case you get lost on you're travels,\n" +
                     "you use the spell word called 'help' or foresee the map from the Fronter realm");
             System.out.println("");
             helpInfo();
-            System.out.println(green + currentRoom.getDescription());
-            System.out.println(blue + "Which direction will you take?" + green);
+            System.out.println(Colours.green + currentRoom.getDescription());
+            System.out.println(Colours.blue + "Which direction will you take?" + Colours.green);
 
             boolean gameOn = true;
             while (gameOn) {
                 userInput = scanner.nextLine().toLowerCase();
                 if ("go north".equals(userInput) || "n".equals(userInput)) {
-                    if (currentRoom.getNorth() == null) {
-                        System.out.println(red + "You cannot go that way \uD83D\uDED1" + green);
-                    } else {
-                        System.out.println("Going north");
-                        currentRoom = currentRoom.getNorth();
-                        System.out.println(currentRoom.getDescription());
-                    }
+                    moveTo(currentRoom.getNorth());
 
                 } else if ("go east".equals(userInput) || "e".equals(userInput)) {
-                    if (currentRoom.getEast() == null) {
-                        System.out.println(red + "You cannot go that way \uD83D\uDED1" + green);
-                    } else {
-                        System.out.println("Going east");
-                        currentRoom = currentRoom.getEast();
-                        System.out.println(currentRoom.getDescription());
-                    }
+                    moveTo(currentRoom.getEast());
 
                 } else if ("go south".equals(userInput) || "s".equals(userInput)) {
-                    if (currentRoom.getSouth() == null) {
-                        System.out.println(red + "You cannot go that way \uD83D\uDED1" + green);
-                    } else {
-                        System.out.println("Going south");
-                        currentRoom = currentRoom.getSouth();
-                        System.out.println(currentRoom.getDescription());
-                    }
+                    moveTo(currentRoom.getSouth());
 
                 } else if ("go west".equals(userInput) || "w".equals(userInput)) {
-                    if (currentRoom.getWest() == null) {
-                        System.out.println(red + "You cannot go that way \uD83D\uDED1" + green);
-                    } else {
-                        System.out.println("Going west");
-                        currentRoom = currentRoom.getWest();
-                        System.out.println(currentRoom.getDescription());
-                    }
+                    moveTo(currentRoom.getWest());
 
                     // andre handlinger
                 } else if ("look".equals(userInput) || "l".equals(userInput)) {
@@ -89,8 +55,17 @@ public class Adventure {
             }
         }
 
+        public void moveTo(Room requestedRoom) {
+            if (requestedRoom == null) {
+                System.out.println(Colours.red + "You cannot go that way \uD83D\uDED1" + Colours.green);
+            } else {
+                currentRoom = requestedRoom;
+                System.out.println(currentRoom.getDescription());
+            }
+        }
+
         public void helpInfo(){
-            System.out.println(yellow + """
+            System.out.println(Colours.yellow + """
                             You can use the following commands: 
                             go North / N
                             go East / E
@@ -99,11 +74,11 @@ public class Adventure {
                             look / L
                             help / H
                             exit / X
-                            """ + green);
+                            """ + Colours.green);
         }
 
         public void exitText(){
-            System.out.println(resetColour + "Couldn´t resist the urge to leave early? \n" +
+            System.out.println(Colours.resetColour + "Couldn´t resist the urge to leave early? \n" +
                     "see you tomorrow \uD83D\uDE31");
         }
 
